d79a91d98878f9268c8da6d0237189bb07218b76
jamesfrienkins <78174595+jamesfrienkins@users.noreply.github.com>
2021-09-30 15:54:55 +0300

    Update graphic.py
    Bugfix.

--- LNCS/build/graphic.py
+++ LNCS/build/graphic.py
@@ -1,5 +1,7 @@
+from typing import Text
 import PySimpleGUI as sg
 import threading
+from PySimpleGUI.PySimpleGUI import running_windows
 import wmi as __wni__
 
 
@@ -32,30 +34,26 @@ class interface:
             self.connected_client = []               # in this massive there info about only CURRENTLY connected clients
             self.clientList_connection = {}          # in this dictionary there are info about displaying clients that are (or was) connected to server (only display info) (ip):(binary value)
             self.clientList_send = {}                # in this dictionary there are info about files for sending in format: (ip):([location_1, location_2])
-            self.clientList_send_display = {}        # in this dictionary there are info about displaying clients that can recieve files (ip):(binary value)
+            self.clientList_send_display = {}        # in this dictionary there are info about displaying clients that can receive files (ip):(binary value)
             self.clientList_request_display = {}     # in this dictionary there are info about displaying clients that wait to send file to server (only display info) (ip):(binary value)
-            self.recieve_request = {}                # in this dictionary there are info about in format: (ip):([location_1, location_2])
-            self.clnt_addr_name = {}                 # in this dictionary there are info about ip addresses and names of clients in format (ip):(name)
+            self.receive_request = {}                # in this dictionary there are info about in format: (ip):([location_1, location_2])
+            self.client_addr_name = {}               # in this dictionary there are info about ip addresses and names of clients in format (ip):(name)
 
             self.dataset = [['addr1', 'name1', 'status1', 'time1'], ['addr2', 'name2', 'status2', 'time2']]
+            self.receive_request = {'addr1':['list1'], 'addr2':['list2']}
 
             for data in self.dataset:
-                self.clnt_addr_name[data[0]] = data[1]
-                self.clientList_connection[data[0]] = data[2]
+                self.client_addr_name[data[0]] = data[1]
 
                 if data[2]:
                     self.connected_client.append(data[0])
 
-        self.recieve_file_layout = [
-        [sg.Text(self.fill('Address')), sg.Text('Name'))]
+        self.receive_file_layout = [
+        [sg.Text(self.fill('Address')), sg.Text('Name')]
         ]
 
         self.send_file_layout = [
-        [sg.Text(self.fill('Address')), sg.Text('Name'))]
-        ]
-
-        self.file_manager_layout = [
-        [sg.TabGroup(    [[sg.Tab('Send file'), self.send_file_layout), sg.Tab('Recieve file', self.recieve_file_layout)]]    )]
+        [sg.Text(self.fill('Address')), sg.Text('Name')]
         ]
 
         self.current_connections_layout = [
@@ -64,48 +62,61 @@ class interface:
         ]
 
         self.current_running_apps_layout = [
-        [sg.Text('Computer: '), sg.Combo(self.connected_client, enable_events=True, key = 'selected_pc_running_tasks'), sg.Text('        Full apps list: '), sg.Checkbox('', key = 'full_app_list')]
+        [sg.Text('Computer: '), sg.Combo(self.connected_client, enable_events=True, key = 'selected_pc_running_tasks'), sg.Button('Get info', key = 'get_client_running_apps'), sg.Text('Full apps list: '), sg.Checkbox('', key = 'full_app_list')]
         ]
 
         for data in self.dataset:               # displaying current connections
             addr, name, stat, tmcn = data
 
             row = [sg.Text(self.fill(addr), key = f'addr:{addr}'), sg.Text('  ' + self.fill(name), key = f'name:{addr}'), sg.Text(self.fill(stat), key = f'stat:{addr}'), sg.Text(self.fill(tmcn), key = f'tmcn:{addr}')]
-
+            
             if self.clientList_connection.get(addr) == None:
                 self.clientList_connection[addr] = True
 
                 self.current_connections_layout.append(row)
+            
+        print(self.connected_client)
 
-        for clnt in self.connected_client:      # displaying recieve file layout
-            files_to_receive = self.recieve_request.get(clnt)
+        for clnt in self.connected_client:      # displaying receive and send file layout
+            files_to_receive = self.receive_request.get(clnt)
+            file_to_receive = ''
+            print(clnt, files_to_receive)
+            
+            if files_to_receive != None:
+                for location in files_to_receive:
+                    file_to_receive += location + '; '
 
-            for location in files_to_receive:
-                file_to_receive += location + '; '
+                row1 = [sg.Text(self.fill(clnt)), sg.Text(self.fill('  ' + self.client_addr_name.get(clnt))), sg.Button('Receive', key = f'receive_file:{clnt}'), sg.Button('Dismiss', key = 'dismiss_file:{clnt}')]
+                row2 = [sg.Text('Folder/file: '), sg.Text(file_to_receive, key = f'file_location_receive:{clnt}')]
 
-            row1 = [sg.Text(self.fill(clnt)), sg.Text(self.fill(self.client_addr_name.get(clnt)), sg.Button('Recieve', key = f'recieve_file:{clnt}), sg.Button('Dismiss', key = 'dismiss_file:{clnt})]
-            row2 = [sg.Text('Folder/file: ), sg.Text('', key = f'file_location:{clnt}')]
+                self.clientList_request_display[clnt] = True
 
-            self.clientList_request_display[clnt] = True
+                self.receive_file_layout.append(row1)
+                self.receive_file_layout.append(row2)
+            
+            file_to_send = self.clientList_send.get(clnt)
 
-            self.recieve_file_layout.append(row1)
-            self.recieve_file_layout.append(row2)
+            if file_to_send == None:
+                file_to_send = ''
 
-        for clnt in self.connected_client:
-            row1 = [sg.Text(self.fill(clnt), sg.Text(self.fill(self.client_addr_name.get(clnt)), sg.Text('')]
-            row2 = [sg.FileBrowser('Select file', key = f'select_file:{clnt}'), sg.FolderBrowser('Select folder', key = f'select_folder:{clnt}')]
+            row1 = [sg.Text(self.fill(clnt)), sg.Text(self.fill('  ' + self.client_addr_name.get(clnt))), sg.Text(''), sg.FolderBrowse('Select folder', key = f'select_folder:{clnt}'), sg.FileBrowse('Select file', key = f'select_file:{clnt}')]
+            row2 = [sg.Text('Folder/file: '), sg.Text(file_to_send, key = f'file_location_send:{clnt}')]
 
             self.clientList_send_display[clnt] = True
 
             self.send_file_layout.append(row1)
             self.send_file_layout.append(row2)
 
+        self.file_manager_layout = [
+        [sg.TabGroup(    [[sg.Tab('Send file', self.send_file_layout), sg.Tab('Receive file', self.receive_file_layout)]],size = (860, 570)    )]
+        ]
+
         self.layout = [
         [sg.Text(f'Current server address: {self.ip}:{self.port}')],
-        [sg.TabGroup(   [[sg.Tab('Connections', self.current_connections_layout), sg.Tab('File manager', self.file_manager_layout), sg.Tab('Running apps', self.current_running_apps_layout)]]   )]
+        [sg.TabGroup(   [[sg.Tab('Connections', self.current_connections_layout), sg.Tab('File manager', self.file_manager_layout), sg.Tab('Running apps', self.current_running_apps_layout)]], size = (880, 585)   )]
         ]
     
-        self.window = sg.Window('LNCS', self.layout)
+        self.window = sg.Window('LNCS', self.layout, size = (900, 600))
 
     def __run_window__(self):
         while True:
